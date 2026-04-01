334a8866560198d162a2b95df487b6dea098c279
Mohammed Rajab <fayez.rajab84@gmail.com>
2026-04-01 12:21:21 +0800

    update-main-tf-remote-state

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~>3.43.0"
     }
   }
+
+  cloud {
+
+    organization = "cybervaultscool"
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
 }
 
 provider "azurerm" {
